db7d706758e0c7e9268fd71681deaeba7a9bd507
p10raveen <p10raveen@gmail.com>
2021-07-30 10:36:04 +0530

    Update 30-July-2021

---
+++ src/tree_implementation/BST.java
@@ -0,0 +1,56 @@
+package tree_implementation;
+
+public class BST {
+
+	Node root = null;
+	
+	void insert(int value)
+	{
+		root = insertValue(root,value); // 8,3
+	}
+	
+	Node insertValue(Node root, int value) // 8,3
+	{
+		if(root==null)
+		{
+			root = new Node(value);
+		}
+		else if(value<root.data)
+		{
+			root.left = insertValue(root.left,value);
+		}
+		else
+		{
+			root.right = insertValue(root.right,value);
+		}
+		return root;
+	}
+	
+	void printTree()
+	{
+		printInorder(root);
+	}
+	
+	void printInorder(Node root) {
+		if (root == null) {
+			return;
+		}
+		printInorder(root.left); // left
+		System.out.print(root.data + "\t"); // root
+		printInorder(root.right); // right
+
+	}
+	
+	public static void main(String[] args) {
+		BST b = new BST();
+		// 8,3,10,1,6,4
+		b.insert(8);
+		b.insert(3);
+		b.insert(10);
+		b.insert(1);
+		b.insert(6);
+		b.insert(4);
+		b.printTree(); // 1 3 4 6 8 10
+	}
+
+}
