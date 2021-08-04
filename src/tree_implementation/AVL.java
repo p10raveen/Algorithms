2b6ddb9ba9d263de5a272bb218872d08b4787596
p10raveen <p10raveen@gmail.com>
2021-08-04 11:06:44 +0530

    Update 04-Aug-2021

---
+++ src/tree_implementation/AVL.java
@@ -0,0 +1,116 @@
+package tree_implementation;
+
+public class AVL {
+
+	Node1 root;
+	int height(Node1 N)
+	{
+		if(N==null)
+			return 0;
+		else
+			return N.height;
+	}
+	
+	int getBalanceFactor(Node1 n)
+	{
+		if(n==null)
+			return 0;
+		else
+			return height(n.left)-height(n.right);
+	}
+	int max(int a,int b)
+	{
+		if(a>b)
+			return a;
+		else
+			return b;
+	}
+	
+	Node1 leftRotate(Node1 a)
+	{
+		Node1 b = a.right;
+		Node1 c = b.left;
+		b.left = a;
+		a.right = c;
+		a.height = max(height(a.left),height(a.right))+1;
+		b.height = max(height(b.left),height(b.right))+1;
+		
+		return b;
+	}
+	
+	Node1 rightRotate(Node1 b)
+	{
+		Node1 a = b.left;
+		Node1 c = a.right;
+		a.right = b;
+		b.left = c;
+		b.height= max(height(b.left),height(b.right))+1;
+		a.height= max(height(a.left),height(a.right))+1;
+		return a;
+	}
+	
+	
+	Node1 insertNode(Node1 node, int value)
+	{
+		if(node==null)
+			return new Node1(value);
+		if(value<node.data)
+			node.left = insertNode(node.left,value);
+		else if(value>node.data)
+			node.right = insertNode(node.right,value);
+		else
+			return node;
+		
+		node.height = max(height(node.left),height(node.right))+1;
+		int balancefactor = getBalanceFactor(node);
+		if(balancefactor>1)
+		{
+			// RR
+			if(value<node.left.data)
+				return rightRotate(node);
+			// RL
+			else if(value>node.left.data)
+				node.left = leftRotate(node.left);
+				return rightRotate(node);
+		}
+		if(balancefactor<-1)
+		{
+			//LL
+			if(value>node.right.data)
+				return leftRotate(node);
+			// LR = RR + LL
+			else if(value<node.right.data)
+				node.right = rightRotate(node.right);
+				return leftRotate(node);
+		}
+		return node;
+	}
+	
+	void inOrder(Node1 node)
+	{
+		if(node != null)
+		{
+			inOrder(node.left);
+			System.out.println(node.data+"\t");
+			inOrder(node.right);
+		}
+	}
+	
+	
+	public static void main(String[] args) {
+		AVL a = new AVL();
+		
+		a.root = a.insertNode(a.root,33);
+		a.root = a.insertNode(a.root,13);
+		a.root = a.insertNode(a.root,53);
+		a.root = a.insertNode(a.root,9);
+		a.root = a.insertNode(a.root,21);
+		a.root = a.insertNode(a.root,61);
+		a.root = a.insertNode(a.root,8);
+		a.root = a.insertNode(a.root,11);
+		
+		a.inOrder(a.root);
+	
+	}
+
+}
---
+++ src/tree_implementation/Node1.java
@@ -0,0 +1,13 @@
+package tree_implementation;
+
+public class Node1 {
+
+	int data,height;
+	Node1 left,right;
+	
+	Node1(int value)
+	{
+		data = value;
+		height = 1;
+	}
+}
